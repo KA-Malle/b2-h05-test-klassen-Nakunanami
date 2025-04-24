ec2aed4ee91cc0417e94008254f82f732f8fd20b
Nakunanami <roxy.sluyts@gmail.com>
2025-04-24 11:02:42 +0200

    uploaden van test H05 klassen

--- Test H5 Klasse Pokemon LEEG/Test H5 Klasse Pokemon LEEG/Form1.Designer.cs
+++ Test H5 Klasse Pokemon LEEG/Test H5 Klasse Pokemon LEEG/Form1.Designer.cs
@@ -43,9 +43,9 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(59, 26);
+            this.label1.Location = new System.Drawing.Point(66, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(128, 32);
+            this.label1.Size = new System.Drawing.Size(146, 38);
             this.label1.TabIndex = 0;
             this.label1.Text = "Speler 1";
             // 
@@ -53,80 +53,90 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(349, 26);
+            this.label3.Location = new System.Drawing.Point(393, 32);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 32);
+            this.label3.Size = new System.Drawing.Size(146, 38);
             this.label3.TabIndex = 2;
             this.label3.Text = "Speler 2";
             // 
             // buttonValAan1
             // 
-            this.buttonValAan1.Location = new System.Drawing.Point(65, 108);
+            this.buttonValAan1.Location = new System.Drawing.Point(73, 135);
+            this.buttonValAan1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonValAan1.Name = "buttonValAan1";
-            this.buttonValAan1.Size = new System.Drawing.Size(154, 23);
+            this.buttonValAan1.Size = new System.Drawing.Size(173, 29);
             this.buttonValAan1.TabIndex = 3;
             this.buttonValAan1.Text = "Val speler 2 aan...";
             this.buttonValAan1.UseVisualStyleBackColor = true;
+            this.buttonValAan1.Click += new System.EventHandler(this.buttonValAan1_Click);
             // 
             // buttonValAan2
             // 
-            this.buttonValAan2.Location = new System.Drawing.Point(341, 108);
+            this.buttonValAan2.Location = new System.Drawing.Point(384, 135);
+            this.buttonValAan2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonValAan2.Name = "buttonValAan2";
-            this.buttonValAan2.Size = new System.Drawing.Size(154, 23);
+            this.buttonValAan2.Size = new System.Drawing.Size(173, 29);
             this.buttonValAan2.TabIndex = 4;
             this.buttonValAan2.Text = "Val speler 1 aan...";
             this.buttonValAan2.UseVisualStyleBackColor = true;
+            this.buttonValAan2.Click += new System.EventHandler(this.buttonValAan2_Click);
             // 
             // labelGezondheid1
             // 
             this.labelGezondheid1.AutoSize = true;
-            this.labelGezondheid1.Location = new System.Drawing.Point(62, 75);
+            this.labelGezondheid1.Location = new System.Drawing.Point(70, 94);
             this.labelGezondheid1.Name = "labelGezondheid1";
-            this.labelGezondheid1.Size = new System.Drawing.Size(86, 16);
+            this.labelGezondheid1.Size = new System.Drawing.Size(104, 20);
             this.labelGezondheid1.TabIndex = 5;
             this.labelGezondheid1.Text = "Gezondheid: ";
             // 
             // labelGezondheid2
             // 
             this.labelGezondheid2.AutoSize = true;
-            this.labelGezondheid2.Location = new System.Drawing.Point(352, 75);
+            this.labelGezondheid2.Location = new System.Drawing.Point(396, 94);
             this.labelGezondheid2.Name = "labelGezondheid2";
-            this.labelGezondheid2.Size = new System.Drawing.Size(86, 16);
+            this.labelGezondheid2.Size = new System.Drawing.Size(104, 20);
             this.labelGezondheid2.TabIndex = 6;
             this.labelGezondheid2.Text = "Gezondheid: ";
             // 
             // buttonGenees1
             // 
-            this.buttonGenees1.Location = new System.Drawing.Point(65, 148);
+            this.buttonGenees1.Location = new System.Drawing.Point(73, 185);
+            this.buttonGenees1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonGenees1.Name = "buttonGenees1";
-            this.buttonGenees1.Size = new System.Drawing.Size(154, 23);
+            this.buttonGenees1.Size = new System.Drawing.Size(173, 29);
             this.buttonGenees1.TabIndex = 7;
             this.buttonGenees1.Text = "Genees";
             this.buttonGenees1.UseVisualStyleBackColor = true;
+            this.buttonGenees1.Click += new System.EventHandler(this.buttonGenees1_Click);
             // 
             // buttonGenees2
             // 
-            this.buttonGenees2.Location = new System.Drawing.Point(341, 148);
+            this.buttonGenees2.Location = new System.Drawing.Point(384, 185);
+            this.buttonGenees2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonGenees2.Name = "buttonGenees2";
-            this.buttonGenees2.Size = new System.Drawing.Size(154, 23);
+            this.buttonGenees2.Size = new System.Drawing.Size(173, 29);
             this.buttonGenees2.TabIndex = 8;
             this.buttonGenees2.Text = "Genees";
             this.buttonGenees2.UseVisualStyleBackColor = true;
+            this.buttonGenees2.Click += new System.EventHandler(this.buttonGenees2_Click);
             // 
             // buttonToonPos
             // 
-            this.buttonToonPos.Location = new System.Drawing.Point(203, 198);
+            this.buttonToonPos.Location = new System.Drawing.Point(228, 248);
+            this.buttonToonPos.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.buttonToonPos.Name = "buttonToonPos";
-            this.buttonToonPos.Size = new System.Drawing.Size(175, 23);
+            this.buttonToonPos.Size = new System.Drawing.Size(197, 29);
             this.buttonToonPos.TabIndex = 9;
             this.buttonToonPos.Text = "Toon posities";
             this.buttonToonPos.UseVisualStyleBackColor = true;
+            this.buttonToonPos.Click += new System.EventHandler(this.buttonToonPos_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(556, 242);
+            this.ClientSize = new System.Drawing.Size(642, 335);
             this.Controls.Add(this.buttonToonPos);
             this.Controls.Add(this.buttonGenees2);
             this.Controls.Add(this.buttonGenees1);
@@ -136,8 +146,10 @@
             this.Controls.Add(this.buttonValAan1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Pokemon game";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
---
+++ Test H5 Klasse Pokemon LEEG/Test H5 Klasse Pokemon LEEG/Speler.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_H5_Klasse_Pokemon_LEEG
+{
+    class Speler
+    {
+        // Instantievariabelen
+        private string _naam;
+        private int _gezondheid; // 1 - 100
+        private int _positieX; // 1 - 500
+        private int _positieY; // 1 - 500
+        private bool _isLevend;
+
+      
+        // Propertie
+        public string Naam
+        {
+            get { return _naam; }
+            set { _naam = value; }
+        }
+
+        public int Gezondheid
+        {
+            get { return _gezondheid; }
+            set { _gezondheid = value; }
+        }
+        public int PositieX
+        {
+            get { return _positieX; }
+            set { _positieX = value; }
+        }
+        public int PositieY
+        {
+            get { return _positieY; }
+            set { _positieY = value; }
+        }
+        public bool IsLevend
+        {
+            get { return _isLevend; }
+            set { _isLevend = value; }
+        }
+
+
+        // Constructor
+
+        // Instantievariablen initialiseren met default waarde 
+        public Speler()
+        {
+            _naam = "Naamloos" ;
+            _gezondheid = 100 ; 
+            _positieX = 0 ;
+            _positieY = 0 ;
+            _isLevend = true;
+        }
+
+        // Alle properties meegeven als parameter
+        public Speler(string naam, int gezondheid, int positieX, int positieY, bool isLevend)
+        {
+            _naam = naam;
+            _gezondheid = 100;
+            _positieX = positieX;
+            _positieY = positieY;
+            _isLevend = true;
+        }
+
+
+        // Methoden
+
+        // X en Y positie wijzigen (enkel positief, optellen)
+        public void Verplaats(int waardeX, int waardeY)
+        {
+            _positieX += waardeX;
+            _positieY += waardeY;
+        }
+
+        // Gezondheid beschadigen (Kan niet negatief worden, anders ISlevend false)
+        public void OntvangSchade(int schade)
+        {
+            _gezondheid -= schade;
+
+            if (_gezondheid <= 0)
+            {
+                _gezondheid = 0;
+                _isLevend = false;
+            }
+        }
+        // Gezondheid herstelen (Kan niet groter dan 100 worden)
+        public void Genees(int herstel)
+        {
+            if ((_gezondheid + herstel) >= 100)
+            {
+                _gezondheid = 100;
+                return;
+            }
+            else
+            {
+                _gezondheid += herstel;
+                return;
+            }
+            
+        }
+
+    }
+}
+
